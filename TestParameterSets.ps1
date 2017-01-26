17a07aac35bd4e18cd24110a4a476d85914109fc
Marco Mansi <marco@mansi.nl>
2017-01-26 14:42:24 +0100

    Revert "Added parameters sets and the creation of RG if they not exist"
    This reverts commit 8b7e4ca089fa2ec59354e6e4b4c7297399be53c5.

--- TestParameterSets.ps1
+++
@@ -1,8 +0,0 @@
-﻿# CreateVSTSPrincipalWithoutResourceGroup
-.\createvstsprincipal.ps1 -subscriptionName "Xpirit - Visual Studio Enterprise with MSDN" -applicationName pasta -password test
-
-# CreateVSTSPrincipalWithExistingResourceGroups
-.\createvstsprincipal.ps1 -subscriptionName "Xpirit - Visual Studio Enterprise with MSDN" -applicationName pasta -password test -resourceGroupNames Bestaat,BestaatNIET
-
-# CreateVSTSPrincipalAndResourceGroups
-.\createvstsprincipal.ps1 -subscriptionName "Xpirit - Visual Studio Enterprise with MSDN" -applicationName pasta -password test -resourceGroupNames Bestaat,BestaatNIET -createReasourceGroups -location "West Europe"

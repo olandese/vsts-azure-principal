8b7e4ca089fa2ec59354e6e4b4c7297399be53c5
Marco Mansi <marco@mansi.nl>
2017-01-26 14:42:24 +0100

    Added parameters sets and the creation of RG if they not exist

---
+++ TestParameterSets.ps1
@@ -0,0 +1,8 @@
+﻿# CreateVSTSPrincipalWithoutResourceGroup
+.\createvstsprincipal.ps1 -subscriptionName "Xpirit - Visual Studio Enterprise with MSDN" -applicationName pasta -password test
+
+# CreateVSTSPrincipalWithExistingResourceGroups
+.\createvstsprincipal.ps1 -subscriptionName "Xpirit - Visual Studio Enterprise with MSDN" -applicationName pasta -password test -resourceGroupNames Bestaat,BestaatNIET
+
+# CreateVSTSPrincipalAndResourceGroups
+.\createvstsprincipal.ps1 -subscriptionName "Xpirit - Visual Studio Enterprise with MSDN" -applicationName pasta -password test -resourceGroupNames Bestaat,BestaatNIET -createReasourceGroups -location "West Europe"
